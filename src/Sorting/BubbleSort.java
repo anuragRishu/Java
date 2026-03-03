575baa42bacc833e75b16c7ea6ace86194481f3f
Anurag Chaurasiya <ai.anu6261@gmail.com>
2026-03-04 01:43:29 +0530

    Bubble Sort

--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -1,31 +1,21 @@
 package Sorting;
 
-import java.util.Scanner;
+public class BubbleSort{
+    public static void main(String a[]){
+        int numbers[] = {8,5,3,6,9,1};
 
-public class BubbleSort {
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int arr[] = new int[4];
-//array input---------------
-        for(int i=0;i<arr.length;i++){
-                arr[i]=sc.nextInt(); }
-        System.out.println("Original Array");
-        for(int i=0;i<arr.length;i++){
-            System.out.println(arr[i]);}
-//bubble sort---------------
-        for(int i=0;i<arr.length-1;i++){
-            for(int j=0;j<arr.length-i-1;j++){
-                if(arr[j]>arr[j+1]){
-           //------swap------
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                }}}
-//array output--------------
-        System.out.println("sorted Array");
-        for(int i=0;i<arr.length;i++){
-            System.out.println(arr[i]);
+        for(int i=0;i<numbers.length-1;i++){
+            for(int j=0;j<numbers.length-i-1;j++){
+                if(numbers[j]>numbers[j+1]){
+                    int temp= numbers[j];
+                    numbers[j]=numbers[j+1];
+                    numbers[j+1]=temp;
+                }
+            }
         }
 
+        for(int i=0;i<numbers.length;i++){
+            System.out.println(numbers[i]);
+        }
     }
 }
